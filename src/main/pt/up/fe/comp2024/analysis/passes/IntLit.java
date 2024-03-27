38f8030a45db5b57604f297e4a49b87aedd9c4b6
miguelsantos42 <up202008450@up.pt>
2024-03-27 15:35:52 +0000

    few more function done

--- src/main/pt/up/fe/comp2024/analysis/passes/IntLit.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/IntLit.java
@@ -1,17 +1,23 @@
 package pt.up.fe.comp2024.analysis.passes;
 
+import org.antlr.v4.runtime.atn.SemanticContext;
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.JmmNode;
-import pt.up.fe.comp2024.analysis.AnalysisVisitor;
+import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2024.analysis.AnalysisPosVisitor;
 import pt.up.fe.comp2024.ast.Kind;
+import pt.up.fe.comp2024.ast.NodeUtils;
 
-public class IntLit extends AnalysisVisitor {
+public class IntLit extends AnalysisPosVisitor {
 
     @Override
     public void buildVisitor() {
         addVisit(Kind.INTEGER_LITERAL, this::visitIntLit);
         addVisit(Kind.BINARY_EXPR, this::visitBinaryExpr);
-        addVisit(Kind.TYPE, this::getType);
+        addVisit("LogicalExpr", this::visitLogicalExpr);
+        addVisit("BooleanLiteral", this::visitBoolLit);
     }
 
     private Void visitIntLit(JmmNode node, SymbolTable table){
@@ -20,18 +26,36 @@ public class IntLit extends AnalysisVisitor {
     }
 
     private Void visitBinaryExpr(JmmNode node, SymbolTable table){
-        JmmNode left =  node.getChildren().get(0);
-        JmmNode right = node.getChildren().get(1);
-
-        String lefttype = getType(left, table);
-        String righttype = getType(right, table);
-
         node.put("type", "int");
-
         return null;
     }
 
-    private String getType(JmmNode node, SymbolTable table) {
-        return node.get("type");
+    private Void visitLogicalExpr(JmmNode node, SymbolTable table){
+        node.put("type", "boolean");
+        if(node.get("op").equals("&&") && (!node.getChild(0).get("type").equals("boolean")||(!node.getChild(1).get("type").equals("boolean")))){
+            String message = "Invalid AND operation between types!";
+            addReport(Report.newError(
+                    Stage.SEMANTIC,
+                    NodeUtils.getLine(node),
+                    NodeUtils.getColumn(node),
+                    message,
+                    null));
+        }
+        else if(node.get("op").equals("<") && (!node.getChild(0).get("type").equals("int")||(!node.getChild(1).get("type").equals("int")))){
+            String message = "Invalid LT operation between types!";
+            addReport(Report.newError(
+                    Stage.SEMANTIC,
+                    NodeUtils.getLine(node),
+                    NodeUtils.getColumn(node),
+                    message,
+                    null));
+        }
+        return null;
     }
+
+    private Void visitBoolLit(JmmNode node, SymbolTable table){
+        node.put("type", "boolean");
+        return null;
+    }
+
 }
